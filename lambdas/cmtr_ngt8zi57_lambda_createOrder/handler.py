64fe4fe412c1747d5dce9ae79b79d006a069e492
ngt8zi57 <ngt8zi57@cloud-mentor.local>
2026-09-22 12:29:20 +0000

    ci: verification 20260922122919

--- lambdas/cmtr_ngt8zi57_lambda_createOrder/handler.py
+++ lambdas/cmtr_ngt8zi57_lambda_createOrder/handler.py
@@ -14,7 +14,7 @@ def lambda_handler(event, context):
 
     table.put_item(
         Item={
-    "verification": "20260922121117",
+    "verification": "20260922122919",
             "orderId": order_id,
             "userId": "mock-user",
             "productId": "mock-product",
